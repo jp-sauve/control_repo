13d56516cee613cbd9d9d6e0df9c45db8787f770
Jean-Paul Sauve <jean-paul@grokology.ca>
2017-12-24 08:32:21 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,7 @@
 node default {
-  file {'/root/README':
+  file { '/root/README':
     ensure => file,
     content => 'This is a readme',
-    owner => 'root',
-  }
-  file {'/root/testfile.txt':
-    ensure => file,
-    content => 'test content',
+    owner   => 'root',
   }
 }
