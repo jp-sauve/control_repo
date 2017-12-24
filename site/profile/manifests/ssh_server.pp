ffc1b1f96b52f2d1f40402bbdf8033ae14f7f33d
Jean-Paul Sauve <jean-paul@grokology.ca>
2017-12-24 14:30:52 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -11,4 +11,5 @@ class profile::ssh_server {
     user => 'root',
     type => 'ssh-rsa',
     key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDAglaBj/i2QEreP7c99nlCyRBbRASjv9hm1Af6CXAiA7QFLPZfR7k2DSBgsZobo0C4HH6HYsAvcGXo4z9ey89lCxPWG49TrA4ksz9+LsHrGxW4VLwOzoSz4jGlgBQbLfGNeB74cwXSiHcCl8/UhhbShi3m5hyRhhDJj/zMqwUGiylam7Z2yd7KqHvHE4m8JIbd33kmhkJIH/zHWigCNWt/L+bg+DaQvG53NzQnOPD308G5f+TEPGpkS1KyCCGrGH+9oSMGCxfek7dqMMGeejgzte3hK0PkYgF/iC38YXTaicd4i9arBetrijTq7ja/OVZfM8EPgxzo/MkbnHLHpyM1',
+  }
 }
